9dcf9c58548ab2064c2119e6aaeb582284a8cdaa
Muhammad <Muhammad124711@gmail.com>
2026-01-29 02:22:02 +0500

    Refactor Footer component with client directive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -106,7 +106,7 @@ export default function Footer() {
                     </h1>
                     <div className="text-zinc-300 text-xs uppercase tracking-widest text-center md:text-right">
                         <span>© 2026. THE ART OF RADIANCE.</span> <br />
-                        <span className="mt-1 block opacity-50">DESIGNED BY <a href="techdxon.com">TECHDXON</a></span>
+                        <span className="mt-1 block opacity-50">DESIGNED BY <a className="underline" href="techdxon.com">TECHDXON</a></span>
                     </div>
                 </div>
             </div>
@@ -114,4 +114,5 @@ export default function Footer() {
         </footer>
     );
 }
+
 
